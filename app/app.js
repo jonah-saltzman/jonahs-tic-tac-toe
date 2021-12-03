7ec7cf15652daf55a606575f0fd3bd2e25dfe08b
jonahsaltzman <jonahsaltzman@gmail.com>
2021-12-03 13:07:55 -0500

    broken auth sections

--- app/app.js
+++ app/app.js
@@ -12,24 +12,29 @@ const testGame = require('./testing')
 // require('./example')
 
 $(() => {
-    // $('#game-section').hide(0)
-    // $('.box').on('click', gameUI.attemptMove)
+    const myModal = new Modal($('#exampleModal'))
+    $('#launch-authModal').on('click', (event) => {
+      event.preventDefault()
+      myModal.show()
+    })
+
+    // Initial hidden elements
     $('#reset-game-btn').hide(0)
     $('#signup').hide(0)
     $('#signin').hide(0)
     $('#signed-in-account').hide(0)
     $('#signout-button').hide(0)
+
+    // On-clicks
     $('#reset-game-btn').on('click', gameEvents.onResetGame)
     $('.box').on('click', gameEvents.onBoardClick)
     $('#start-game-btn').on('click', gameEvents.onNewGame)
     $('#launch-authModal').on('click', authEvents.onSigninButton)
-    $('#signup-form').on('submit', authEvents.onRegister)
-    $('#signin-form').on('submit', authEvents.onSigninSubmit)
     $('#signin-register').on('click', authEvents.onSigninRegisterButton)
     $('#register-signin').on('click', authEvents.onRegisterSigninButton)
     $('#signout-button').on('click', authEvents.onSignout)
-    if (testGame) {
-      $('#game-section').show(0)
-      $('#reset-game-btn').hide(0)
-    }
+
+    // On-submits
+    $('#signup-form').on('submit', authEvents.onRegister)
+		$('#signin-form').on('submit', authEvents.onSigninSubmit)
 })
